638df55e06635c4bd6b5dedba542222ea7a93492
Xyberista <50119873+Xyberista@users.noreply.github.com>
2022-01-20 14:37:08 -0600

    read input function

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Description
+
+Rust solution for 2022 Google Hashcode Practice Round.
+Put input in "input" folder in project root and provide it via command line
+or in program.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io::{prelude::*, self}};
+
+const INPUT_DIRECTORY: &str = "./input/";
+const DEFAULT_INPUT: &str = "a_an_example.in.txt";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+    Ok(())
+}
+
+/// Gets input from default directory. 
+/// Filename is passed through command line, and defaults to the constant [DEFAULT_INPUT].
+fn get_input() -> Result<String, io::Error> {
+    let args = std::env::args().collect::<Vec<String>>();
+    let filename = if let Some(filename) = args.get(1) {
+        filename
+    } else {
+        DEFAULT_INPUT
+    };
+    fs::read_to_string(format!("{}{}", INPUT_DIRECTORY, filename))
 }
