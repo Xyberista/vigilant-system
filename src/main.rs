472ee235a63c9230c0ca67de0221b973b3dfb0ea
Xyberista <50119873+Xyberista@users.noreply.github.com>
2022-01-20 14:43:17 -0600

    changed way input is read

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,20 @@ const INPUT_DIRECTORY: &str = "./input/";
 const DEFAULT_INPUT: &str = "a_an_example.in.txt";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
+    let mut reader = io::BufReader::new(get_filename()?);
+    let mut buf = String::new();
+    reader.read_line(&mut buf)?;
+    let clients: usize = buf.trim().parse()?;
     Ok(())
 }
 
-/// Gets input from default directory. 
 /// Filename is passed through command line, and defaults to the constant [DEFAULT_INPUT].
-fn get_input() -> Result<String, io::Error> {
+fn get_filename() -> Result<fs::File, io::Error> {
     let args = std::env::args().collect::<Vec<String>>();
     let filename = if let Some(filename) = args.get(1) {
         filename
     } else {
         DEFAULT_INPUT
     };
-    fs::read_to_string(format!("{}{}", INPUT_DIRECTORY, filename))
+    fs::File::open(format!("{}{}", INPUT_DIRECTORY, filename))
 }
