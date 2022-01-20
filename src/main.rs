34d6b1b4db8436badc7ad0b06804bb1aaacc0081
Xyberista <50119873+Xyberista@users.noreply.github.com>
2022-01-20 14:54:45 -0600

    added way to parse each client

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
-use std::{fs, io::{prelude::*, self}};
+use std::{
+    collections::HashSet,
+    fs,
+    io::{self, prelude::*},
+};
 
 const INPUT_DIRECTORY: &str = "./input/";
 const DEFAULT_INPUT: &str = "a_an_example.in.txt";
+
+/// Ingredients type. (Ing short for Ingredients)
+type Ing = HashSet<String>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Client {
+    likes: Ing,
+    dislikes: Ing,
+}
+
+impl Client {
+    fn new(likes: Ing, dislikes: Ing) -> Self {
+        Self { likes, dislikes }
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut reader = io::BufReader::new(get_filename()?);
     let mut buf = String::new();
     reader.read_line(&mut buf)?;
-    let clients: usize = buf.trim().parse()?;
+    let number_of_clients: usize = buf.trim().parse()?;
+    let mut clients: Vec<Client> = Vec::new();
+    for _ in 0..number_of_clients {
+        let mut likes = String::new();
+        reader.read_line(&mut likes)?;
+        let mut likes = likes.split(' ');
+        let _n_likes = likes.next().unwrap();
+        let likes = likes.map(|s| s.to_string()).collect::<HashSet<String>>();
+
+        let mut dislikes = String::new();
+        reader.read_line(&mut dislikes)?;
+        let mut dislikes = dislikes.split(' ');
+        let _n_dislikes = dislikes.next().unwrap();
+        let dislikes = dislikes.map(|s| s.to_string()).collect::<HashSet<String>>();
+
+        clients.push(Client::new(likes, dislikes));
+    }
     Ok(())
 }
 
